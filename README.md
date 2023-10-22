2ab997b4f552e1a66c321d4ebcdebf9001b1cd6e
Gideon Bett <kiprotichgideonbett@gmail.com>
2023-10-22 10:53:16 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Personal portfolio 
+
+##  Main information
+
+My name is Gideon Bett and I'm a Frontend Web developer from Maseno, Kisumu.
+
+My many years of experience in  graphic design allows me to create high-quality, responsive and beautiful websites and applications.\
+I'm a highly motivated person with a passion for the development Front-End. I enjoy bringing static designs to life by creating dynamic, lively and responsive renders that I am proud of.\
+You can check out on me on linkedin and see more web projects on my Github profile.\
+And I'm currently improving my skills in MERN stack.
+
+I design and develop experience that make people's lives simple.
+
+##  My technical skills: 
+
+### Front-End Development:
+- Html5 | CSS3 | Styled Components | PHP | Bootstrap5 | JavaScript 
+
+### Backend Development:
+- Node.js | Express.js | MongoDB | PHP
+
+### Web & Graphic Design: 
+- Adobe Photoshop | Adobe Illustrator | Adobe InDesign | Adobe Lightroom | Publisher
---
+++ script.js
@@ -0,0 +1,80 @@
+// filter
+
+const categoryTitle = document.querySelectorAll('.category-title');
+const allCategoryPosts = document.querySelectorAll('.all');
+
+for(let i = 0; i < categoryTitle.length; i++){
+    categoryTitle[i].addEventListener('click', filterPosts.bind(this, categoryTitle[i]));
+}
+
+function filterPosts(item){
+    changeActivePosition(item);
+    for(let i = 0; i < allCategoryPosts.length; i++){
+        if(allCategoryPosts[i].classList.contains(item.attributes.id.value)){
+            allCategoryPosts[i].style.display = "block";
+        } else {
+            allCategoryPosts[i].style.display = "none";
+        }
+    }
+}
+
+function changeActivePosition(activeItem){
+    for(let i = 0; i < categoryTitle.length; i++){
+        categoryTitle[i].classList.remove('active');
+    }
+    activeItem.classList.add('active');
+};
+
+
+// text animation
+
+const words = ['web developer', 'photoeditor', 'designer']
+
+gsap.to(".cursor", {
+    opacity: 0,
+    repeat: -1,
+    yoyo: true,
+    duration: 0.5,
+    ease: "power2.inOut"
+});
+
+let tlMaster = gsap.timeline({ repeat: -1 });
+
+words.forEach((word) => {
+    let tlText = gsap.timeline({
+        repeat: 1,
+        yoyo: true,
+        repeatDelay: 1
+    });
+    tlText.to(".animated-text", { duration: 1.5, text: word, });
+    tlMaster.add(tlText);
+})
+
+gsap.to("h3", {
+    text: "web developer",
+    duration: 2,
+    repeat: -1,
+    repeatDelay: .7,
+    ease: "power1.in",
+    yoyo: true,
+})
+
+
+// burger menu
+
+let burgerMenu = document.querySelector("#burger-menu");
+let overlay = document.querySelector("#menu");
+
+burgerMenu.addEventListener("click", function(){
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+});
+
+// initializing aos  
+
+AOS.init({
+    delay:400,
+    duration:1000
+})
+
+
